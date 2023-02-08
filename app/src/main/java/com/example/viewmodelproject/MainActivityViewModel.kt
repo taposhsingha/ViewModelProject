a8ce90943515c6c52e32319c14d86f23dd765b7b
taposhsingha <57071916+taposhsingha@users.noreply.github.com>
2023-02-08 19:59:41 +0600

    files added

---
+++ app/src/main/java/com/example/viewmodelproject/MainActivityViewModel.kt
@@ -0,0 +1,10 @@
+package com.example.viewmodelproject
+
+import androidx.lifecycle.ViewModel
+
+class MainActivityViewModel:ViewModel() {
+    var count = 0
+    fun updateCount(){
+        ++count
+    }
+}
